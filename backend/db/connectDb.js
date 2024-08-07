0ffcc5fd871049df40da80a913d92515ff8d24fa
vishwajithBarad1 <vishwajithbarad@gmail.com>
2024-08-07 22:41:01 +0530

    this is revert baked file

--- backend/db/connectDb.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require("mongoose");
-const mongooseUrl = process.env.mongooseUrl
-
-mongoose.connect(mongooseUrl);
-
-mongoose.connection.on("connection",()=>{console.log("mongoose connected successfully")});
-
-module.exports = {mongoose}
